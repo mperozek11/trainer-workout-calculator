bc1d5d759b8e483edfc3159e1de81a6c63f91d3d
maxperozek <maxperozek@gmail.com>
2021-04-29 17:04:40 -0400

    first commit

---
+++ src/main/java/org/perom/trainercalculator/TrainerCalculatorApplication.java
@@ -0,0 +1,13 @@
+package org.perom.trainercalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainerCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainerCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/perom/trainercalculator/TrainerCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.perom.trainercalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainerCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
